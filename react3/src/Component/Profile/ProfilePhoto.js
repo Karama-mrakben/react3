0311aad5b178f0e276f11ffe848cc132e15c5b64
Karama-mrakben <81178763+Karama-mrakben@users.noreply.github.com>
2021-05-11 14:34:45 +0200

    Update ProfilePhoto.js

--- react3/src/Component/Profile/ProfilePhoto.js
+++ react3/src/Component/Profile/ProfilePhoto.js
@@ -1,4 +1,4 @@
 const ProfilePhoto= ()=>{
      return <img src="https://i.pinimg.com/originals/8f/3f/08/8f3f08859a72c40261706b577847d59c.jpg" />
-                        };
+                       };
 export default ProfilePhoto
