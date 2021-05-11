1a93dbb3734024fdc3c5d6caa14e3f6d43e18520
Karama-mrakben <81178763+Karama-mrakben@users.noreply.github.com>
2021-05-11 14:26:16 +0200

    Update ProfilePhoto.js

--- react3/src/Component/Profile/ProfilePhoto.js
+++ react3/src/Component/Profile/ProfilePhoto.js
@@ -1,4 +1,4 @@
 const ProfilePhoto= ()=>{
-                         return <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Faperture.consulting%2Ftestimonials%2Fsample-testimonial-1%2Fanonymous-profile%2F&psig=AOvVaw06Ror7mOtPvV7lTRNI6r2s&ust=1620803464997000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLC5stCJwfACFQAAAAAdAAAAABAD" />
+     return <img src="https://i.pinimg.com/originals/8f/3f/08/8f3f08859a72c40261706b577847d59c.jpg" />
                         };
 export default ProfilePhoto
